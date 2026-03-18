06f3ad0adfdd937152a99d50695f1d8119d0a55b
MrAdamTweedie <adamgtweedie@gmail.com>
2026-03-18 20:54:42 +0000

    update news table

--- jobs/refresh_news.py
+++ jobs/refresh_news.py
@@ -1,11 +1,11 @@
-from services.news_service import fetch_latest_news
+from services.news_service import get_gn_news_by_symbol
 from db import insert_news, get_active_short_names
 
 
 def run_refresh_news():
 
     for short_name in get_active_short_names():
-        for article in fetch_latest_news(short_name):
+        for article in get_gn_news_by_symbol(short_name):
             insert_news(
                 short_name=short_name,
                 title=article["title"],
