7e1164833721d0a087af109d55aba7741219a67e
MrAdamTweedie <adamgtweedie@gmail.com>
2026-03-29 14:18:59 +0100

    update comments

--- scheduler.py
+++ scheduler.py
@@ -5,22 +5,21 @@ from jobs import run_aggregate_sentiment
 
 scheduler = BlockingScheduler()
 
-# refresh stock list once a week, monday at midnight
+# refresh stock list once p/w
 scheduler.add_job(run_refresh_stocks, "cron", day_of_week="mon", hour=0, minute=0)
 
-# update stock prices every 15 minutes
+# update stock prices 15/m
 scheduler.add_job(update_stock_prices, "interval", minutes=60)
 
-# refresh news every hour
+# refresh news 1p/h
 scheduler.add_job(run_refresh_news, "interval", hours=1)
 
-# aggregate sentiment once a day at 11pm
+# aggregate 11pm 1p/d
 scheduler.add_job(run_aggregate_sentiment, "cron", hour=23, minute=0)
 
 if __name__ == "__main__":
     print("Starting scheduler...")
     
-    # run all jobs once immediately on startup
     run_refresh_stocks()
     update_stock_prices()
     run_refresh_news()
