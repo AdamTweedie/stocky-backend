6bcc67f8c133ce4fcd3eed021a37b98b91ab0c57
MrAdamTweedie <adamgtweedie@gmail.com>
2026-03-31 23:30:43 +0100

    update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -2,6 +2,6 @@ from .stock_service import get_instruments, get_stock_price_av, get_stock_price_
 
 from .news_service import get_gn_news_by_symbol, fetch_rss_news
 
-from .sentiment_service import get_sentiment
+from .sentiment_service import get_sentiment, get_bulk_sentiment
 
 from .ai_service import summarise_article, summarise_recent_news
