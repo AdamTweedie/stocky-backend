fdd22de5f6183bfe5690df977d549e1d4e862a7b
MrAdamTweedie <adamgtweedie@gmail.com>
2026-03-31 23:32:26 +0100

    finBERT implementation

--- services/sentiment_service.py
+++ services/sentiment_service.py
@@ -1,10 +1,50 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+"""
+    TODO: import and use 'accelerate'
+"""
 
-analyser = SentimentIntensityAnalyzer()
+from transformers import pipeline
+import torch
+
+_classifier = None
+
+def _get_classifier():
+    global _classifier
+    if _classifier is None:
+        print("[sentiment] Loading FinBERT model...")
+        device = 0 if torch.cuda.is_available() else -1
+        _classifier = pipeline(
+            "sentiment-analysis",
+            model="ProsusAI/finbert",
+            truncation=True,
+            max_length=512,
+            batch_size=16,
+            device=device
+        )
+        print("[sentiment_service] ProsusAI/finbert model loaded")
+    return _classifier
+
+
+def _label(result):
+    label = result["label"]
+    score = result["score"]
+
+    if label == "positive":
+        return round(score, 3)
+    elif label == "negative":
+        return -round(score, 3)
+    else:
+        return 0
+
 
 def get_sentiment(text: str) -> float:
-    scores = analyser.polarity_scores(text)
-    if scores['compound'] is None:
-        return 0
-    else:
-        return round(scores['compound'], 3) # returns -1.0 (negative) to 1.0 (positive)
+    print(f"[get_sentiment] calculating sentiment for 1 article")
+    result = _get_classifier()(text)[0]
+    return _label(result)
+
+
+def get_bulk_sentiment(texts: list[str]) -> list[float]:
+    print(f"[get_bulk_sentiment] calculating sentiment for {len(texts)} articles")
+    results = _get_classifier()(texts)
+    return [_label(r) for r in results] 
+
+
